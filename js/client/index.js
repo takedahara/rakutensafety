b1f3291eba9d93f084ae5939bff883bbd6eac629
Tomoya-Kouno-Laboratory <kouno.tomoya.528@s.kyushu-u.ac.jp>
2024-08-06 15:07:07 +0900

    succeed test

--- js/client/index.js
+++ js/client/index.js
@@ -1,4 +1,14 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('listening on port 3000!'))
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Viewエンジンの設定
+app.set('view engine', 'ejs');
+
+// ルートの設定
+const indexRouter = require('./routes/route');
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
